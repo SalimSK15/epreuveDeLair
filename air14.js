8a6851899c21d89bec3be10971fd34ff0f959a12
salim <salimdu15@outlook.fr>
2023-03-23 03:20:24 +0100

    exo14 AIR Ok

---
+++ air14.js
@@ -0,0 +1,4 @@
+const chalk = require('chalk');
+const log = console.log;
+
+console.log(chalk.white.bold("jai en fin fini")+chalk.black.bgGreen.bold(" l'epreuve de l'air")+chalk.grey.bgRed.white.italic(" et c'etait un ouuuf oyayaaay"));
